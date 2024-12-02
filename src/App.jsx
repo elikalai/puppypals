43a53d3a38de1fe05d383dfc304fabedd554d355
elikalai <elikalaixiv@gmail.com>
2024-12-02 13:09:24 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import {puppyList} from './data.js'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);
+
+  return (
+    
+      <div className="App">
+        
+        {puppies.map((puppy) => (
+        <p 
+          key={puppy.id} 
+          onClick={() => {
+            console.log("puppy id clicked:", puppy.id);
+            setFeatPupId(puppy.id); 
+          }}
+        >
+          {puppy.name}
+        </p>
+      ))}
+        
+        { featPupId && (
+          <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+          </div>
+        
+        )}
+      </div>
+      
+    
+  );
+}
+
+export default App
